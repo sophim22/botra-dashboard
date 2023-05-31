684a64840d5b2e0251483696be729a24baf0e519
sophim22 <sophimkonmak198@gmail.com>
2023-05-31 23:04:01 +0700

    done-auth

--- src/config/sms.js
+++ src/config/sms.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 import Twilio from "twilio";
 
 export const sendOTPSMS = async (phone, templates) => {
-  if (process.env.APP_ENV !== "production") {
+  if (process.env.APP_ENV === "production") {
     const twilio = Twilio(process.env.ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
     await twilio.messages.create({
       body: templates,
